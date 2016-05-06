76cdee50fc8ad005945d8b073034a240b423f5a2
GorA <goraga1@github.com>
2016-05-06 15:40:45 +0400

    first commit

---
+++ app/src/main/java/com/whyinside/whyinside/MyApplication.java
@@ -0,0 +1,14 @@
+package com.whyinside.whyinside;
+
+import android.app.Application;
+
+/**
+ * Created by GorA on 5/6/16.
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
